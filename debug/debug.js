ed3f92cda80abd6011384cca18e7553c5e1576c8
LeungZ9 <31878600+LeungZ9@users.noreply.github.com>
2021-03-05 17:01:40 +0800

    feat: debug vue runtimecore

--- debug/debug.js
+++ debug/debug.js
@@ -1,4 +1,5 @@
 import {effect, ref, reactive} from '@vue/reactivity'
+import { createVNode as _createVNode, Fragment as _Fragment, openBlock as _openBlock, createBlock as _createBlock } from '@vue/runtime-core'
 
 window.msg = ref('hello')
 window.check = ref(true)
@@ -10,5 +11,13 @@ effect(() => {
     console.log('track')
 })
 
-debugger
 check.value = false
+
+function render(_ctx, _cache, $props, $setup, $data, $options) {
+  return (_openBlock(), _createBlock(_Fragment, null, [
+    _createVNode("div", null, "Hello World!"),
+    _createVNode("div", null, "dwadwad")
+  ], 64 /* STABLE_FRAGMENT */))
+}
+
+console.log(render())
